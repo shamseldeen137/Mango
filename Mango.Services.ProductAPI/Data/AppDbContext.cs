9678fac5c1cee73a2b46e875f69e9de88d1e6664
shamseldeen137 <shams.eldeen.ali@outlook.com>
2025-08-30 12:52:42 +0300

    test push product api azure ci cd

--- Mango.Services.ProductAPI/Data/AppDbContext.cs
+++ Mango.Services.ProductAPI/Data/AppDbContext.cs
@@ -8,6 +8,7 @@ namespace Mango.Services.ProductAPI.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+
         public DbSet<Product> Products { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
